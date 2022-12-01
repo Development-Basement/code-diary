dacaf2e3dcf6fc381dda6cef0af1399c798444a3
xhrnca00 <89995247+xhrnca00@users.noreply.github.com>
2022-12-01 22:33:41 +0100

    create database schema, start writing rules

---
+++ designFiles/database.schema.ts
@@ -0,0 +1,52 @@
+// $prefix -> generic over following name
+// _prefix -> a collection
+
+type Post = {
+  date: EpochTimeStamp; // timestamp(firebase type)
+  language: string; // programming language name
+  minutesSpent: number; // number of minutes spent
+  rating: number; // 0 - 5 stars
+  description: string; // a description of what the programmer did
+  tags: Array<string>; // category/tag names
+};
+
+type Tag = {
+  tagColor: string; // HTML/HEX color code
+  name: string; // name of the tag, probably /[a-z-]/, 3 - 13 chars or so
+  description: string; // tag description
+};
+
+export type DB = {
+  users: {
+    allUsers: {
+      users: Map<string, string>; // UUID -> Username
+    };
+    $uuid: {
+      username: string;
+      groups: Array<string>; // GroupID
+      profileColor: string; // HTML/HEX color code
+    };
+  };
+  groups: {
+    $group: {
+      allMembers: Array<string>; // UUID
+      admins: Array<string>; //UUID
+      _$uuid: {
+        postId: Post;
+      };
+      _tags: {
+        $tag: Tag;
+      };
+    };
+  };
+  userPosts: {
+    $uuid: {
+      _posts: {
+        postId: Post;
+      };
+      _tags: {
+        $tag: Tag;
+      };
+    };
+  };
+};
