ce86eb7a146ba5db70b2e5c84f9ad95551286518
Albert Pátík <albert.patik.main@gmail.com>
2022-12-05 19:36:37 +0100

    Finish Auth Context

--- contexts/AuthContext.tsx
+++ contexts/AuthContext.tsx
@@ -5,6 +5,7 @@ import {
   collection,
   doc,
   getDocs,
+  getDoc,
   setDoc,
   updateDoc,
   query,
@@ -12,7 +13,11 @@ import {
   where,
   deleteDoc,
 } from "firebase/firestore";
+
+import { useAuthState } from "react-firebase-hooks/auth";
+
 import {
+  getAuth,
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
   signOut,
@@ -22,7 +27,9 @@ import {
   EmailAuthProvider,
   reauthenticateWithCredential,
 } from "firebase/auth";
-import { JsxElement } from "typescript";
+
+import firebaseApp from "../lib/firebase";
+import { Auth } from "firebase-admin/auth";
 
 const AuthContext = React.createContext({});
 
@@ -30,268 +37,101 @@ export function useAuth() {
   return useContext(AuthContext);
 }
 
-export function AuthProvider({ children }: JsxElement) {
-  const [currentUser, setCurrentUser] = useState(null);
-  const [userData, setUserData] = useState(null);
-  const [dataUnsub, setDataUnsub] = useState(null);
+export function AuthProvider({ children }: { children: JSX.Element }) {
+  const auth = getAuth(firebaseApp);
 
-  const [loadingAccount, setLoadingAccount] = useState(true);
-  const [loadingData, setLoadingData] = useState(false);
-  const [loadingNewAccount, setLoadingNewAccount] = useState(false);
+  const [user, loading, error] = useAuthState(auth);
 
   const usersRef = collection(db, "users");
-  const deletedUsersRef = collection(db, "deletedUsers");
 
-  async function findDocumentIdByUsername(username) {
-    const q = query(usersRef, where("username", "==", username));
-    let response = "";
-    await getDocs(q).then((users) => {
-      users.forEach((user) => {
-        response = user.id;
-      });
-    });
-    return response;
+  type basicUserDataType = {
+    email: string;
+    password: string;
+  };
+
+  interface createUserDataType extends basicUserDataType {
+    username: string;
   }
 
-  async function findUserEmailByUsername(username) {
-    const q = query(usersRef, where("username", "==", username));
-    let email = "";
-    await getDocs(q).then((docs) => {
-      docs.forEach((user) => {
-        email = user.data().email;
-      });
-    });
-    return email;
-  }
-
-  async function isEmailInUserDatabase(email) {
-    const q = query(usersRef, where("email", "==", email));
-    let response = false;
-    await getDocs(q).then((docs) => {
-      response = !docs.empty;
-    });
-    return response;
-  }
-
-  async function isUsernameInUserDatabase(username) {
-    const q = query(usersRef, where("username", "==", username));
-    let response = false;
-    await getDocs(q).then((docs) => {
-      response = !docs.empty;
-    });
-    return response;
-  }
-
-  async function isEmailInDeletedDatabase(email) {
-    const q = query(deletedUsersRef, where("email", "==", email));
-    let response = false;
-    let uid = "";
-    await getDocs(q).then((docs) => {
-      response = !docs.empty;
-      if (response) {
-        docs.forEach((_doc) => {
-          uid = _doc.data().uid;
-        });
-      }
-    });
-    return { response, uid };
-  }
-
-  async function createFirstAccount(email, username, password) {
+  async function createAccount({
+    email,
+    username,
+    password,
+  }: createUserDataType) {
     await createUserWithEmailAndPassword(auth, email, password).then(
       async (response) => {
-        await setDoc(doc(db, "users", response.user.uid), {
+        await setDoc(doc(db, "users/accountInfo/_public/", response.user.uid), {
           email,
           username,
-          roles: ["manazer"],
         });
       },
     );
   }
 
-  async function createAnotherAccount(
-    newEmail,
-    newUsername,
-    newRoles,
-    managerPassword,
-    bounceTo = "/manage-accounts",
-  ) {
-    let willReload = false;
-    let isDeleted;
-    await Promise.all([
-      isUsernameInUserDatabase(newUsername),
-      isEmailInUserDatabase(newEmail),
-      isEmailInDeletedDatabase(newEmail),
-    ]).then((responses) => {
-      let nameInDb = responses[0];
-      let emailInDb = responses[1];
-      let _isDeleted = responses[2];
-      if (nameInDb) throw new Error(`Username already in database ${nameInDb}`);
-      if (emailInDb) throw new Error(`Email already in database ${emailInDb}`);
-      isDeleted = _isDeleted;
-    });
-    let newUid;
-    if (isDeleted.response) {
-      newUid = isDeleted.uid;
-      await deleteDoc(doc(db, "deletedUsers", isDeleted.uid));
-    } else {
-      willReload = true;
-      let managerEmail = userData.email;
-      let newPassword = uuidv4();
-      setLoadingNewAccount(true);
-      await createUserWithEmailAndPassword(auth, newEmail, newPassword).then(
-        (response) => {
-          newUid = response.user.uid;
-        },
-      );
-      await signOut(auth);
-      await signInWithEmailAndPassword(auth, managerEmail, managerPassword);
-      setLoadingNewAccount(false);
-      navigate(bounceTo);
-    }
-    await setDoc(doc(db, "users", newUid), {
-      email: newEmail,
-      username: newUsername,
-      roles: newRoles,
-    });
-    await sendPasswordResetEmail(auth, newEmail);
-    return willReload;
-  }
-
-  async function deleteUser(username) {
-    let uid, email;
-    await Promise.all([
-      findDocumentIdByUsername(username),
-      findUserEmailByUsername(username),
-    ]).then((responses) => {
-      uid = responses[0];
-      email = responses[1];
-    });
-    if (!uid || !email) return;
-    await Promise.all([
-      deleteDoc(doc(db, "users", uid)),
-      setDoc(doc(db, "deletedUsers", uid), {
-        email,
-        uid,
-      }),
-    ]);
-  }
-
-  async function login(username, password) {
-    await findUserEmailByUsername(username).then(async (email) => {
-      if (email) await signInWithEmailAndPassword(auth, email, password);
-      else throw new Error("Username not in database");
-    });
+  async function login({ email, password }: basicUserDataType) {
+    if (email) await signInWithEmailAndPassword(auth, email, password);
   }
 
   async function logout() {
     await signOut(auth);
   }
 
-  async function resetPassword(email) {
-    if (await isEmailInUserDatabase(email)) {
+  async function resetPassword(email: string) {
+    try {
       await sendPasswordResetEmail(auth, email);
-    } else {
+    } catch (error) {
       throw new Error("User not in database");
     }
   }
 
-  async function reauthenticate(password) {
-    const credential = EmailAuthProvider.credential(
-      currentUser.email,
-      password,
-    );
-    await reauthenticateWithCredential(currentUser, credential);
-  }
-
-  async function changePassword(newPassword) {
+  async function changePassword(newPassword: string) {
     // should only be called by current user and shortly after authenticating
-    await updatePassword(currentUser, newPassword);
+    if (user) await updatePassword(user, newPassword);
   }
 
-  async function changeUsername(currentUsername, newUsername) {
+  async function changeUsername(newUsername: string) {
     if (await isUsernameInUserDatabase(newUsername)) {
       throw new Error("Username already in database");
     }
-    const userDocId = await findDocumentIdByUsername(currentUsername);
-    if (userDocId) {
-      await updateDoc(doc(db, "users", userDocId), {
-        username: newUsername,
-      });
-    } else {
-      throw new Error("User not in database");
+    if (user) {
+      const userDoc = await getDoc(
+        doc(db, "users/accountInfo/_public/", user.uid),
+      );
+      if (userDoc) {
+        await updateDoc(doc(db, "users/accountInfo/_public/", userDoc.id), {
+          username: newUsername,
+        });
+      } else {
+        throw new Error("User not in database");
+      }
     }
   }
 
-  async function changeRoles(username, newRoles) {
-    const uid = await findDocumentIdByUsername(username);
-    if (!uid) throw new Error("User not in database");
-    await updateDoc(doc(db, "users", uid), { roles: newRoles });
-  }
-
-  function hasRole(role) {
-    if (currentUser && userData) {
-      return userData.roles.includes(role);
-    } else return false;
-  }
-
-  useEffect(() => {
-    return onAuthStateChanged(auth, (user) => {
-      setLoadingAccount(true);
-      setCurrentUser(user);
-      console.log("loggedIn:%o", !!user);
-      setLoadingAccount(false);
+  async function isUsernameInUserDatabase(username: string) {
+    const q = query(usersRef, where("username", "==", username));
+    let response = false;
+    await getDocs(q).then((docs) => {
+      response = !docs.empty;
     });
-  }, []);
-
-  useEffect(() => {
-    // setLoading(false) twice. First one is a promise, so it gets executed later
-    setLoadingData(true);
-    if (dataUnsub) {
-      dataUnsub.unsub();
-      setDataUnsub(null);
-    }
-    if (currentUser) {
-      let unsub = onSnapshot(doc(db, "users", currentUser.uid), (response) => {
-        console.log("updated user data");
-        if (!response.exists()) {
-          logout();
-          return;
-        }
-        setUserData(response.data());
-        setLoadingData(false);
-      });
-      setDataUnsub({ unsub: unsub });
-    } else {
-      setUserData(null);
-      setLoadingData(false);
-    }
-  }, [currentUser]);
+    return response;
+  }
 
   const value = {
-    currentUser,
-    userData,
+    user,
+    createAccount,
     login,
-    createFirstAccount,
     logout,
     resetPassword,
-    hasRole,
-    reauthenticate,
-    changeUsername,
     changePassword,
-    changeRoles,
-    createAnotherAccount,
-    deleteUser,
+    changeUsername,
   };
 
   return (
     <AuthContext.Provider value={value}>
-      {!loadingAccount && !loadingData && !loadingNewAccount && children}
-      {loadingNewAccount && (
-        <h1 className="text-center justify-content-center my-auto">
-          Přidávám účet...
-        </h1>
-      )}
+      <>{!loading && !error && user && children}</>
+      <>{error && <p>error</p>}</>
+      <>{loading && <p>loading</p>}</>
+      <>{!user && <p>no user</p>}</>
     </AuthContext.Provider>
   );
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,16 @@
 import "@styles/globals.css";
 import type { AppProps } from "next/app";
 import { ThemeContextProvider } from "../contexts/ThemeContext";
+import { AuthProvider } from "../contexts/AuthContext";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <ThemeContextProvider>
-        <Component {...pageProps} />
-      </ThemeContextProvider>
+      <AuthProvider>
+        <ThemeContextProvider>
+          <Component {...pageProps} />
+        </ThemeContextProvider>
+      </AuthProvider>
     </>
   );
 }
