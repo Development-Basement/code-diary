bebb905a8c53d59b34d2f12637221bf3e336217f
Albert Pátík <albert.patik.main@gmail.com>
2022-12-13 15:24:59 +0100

    Add New Category Logic
    Different Sidebar Layout

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -3,16 +3,55 @@ import { possibleThemes, ThemeContext } from "@contexts/themeContext";
 
 import { FC, useContext } from "react";
 
+import { Add } from "@mui/icons-material";
+
 const Sidebar: FC = () => {
   const { userData } = useAuth();
   const { setTheme } = useContext(ThemeContext);
 
+  const addNewTeamHandle = () => {
+    console.log("Adding new team..."); // REMOVE LATER
+  };
+
+  const addNewCategoryHandle = () => {
+    console.log("Adding new category..."); // REMOVE LATER
+  };
+
   return (
     <div className="flex h-full w-64 flex-col bg-neutral p-3 text-neutral-content">
+      <input type="checkbox" id="my-modal" className="modal-toggle" />
+      <label htmlFor="my-modal" className="modal cursor-pointer">
+        <label className="modal-box relative" htmlFor="">
+          <h3 className="text-lg font-bold">Add new Note</h3>
+        </label>
+      </label>
+
       <nav className="flex flex-col">
-        <h2 className="text-3xl">Personal</h2>
-        <span className="divider" />
-        <h2 className="text-3xl">Teams</h2>
+        <div className="mb-8 flex h-min w-full flex-col">
+          <span className="flex flex-row items-center justify-between">
+            <h2 className="text-3xl">Teams</h2>
+            <Add
+              className="hover:cursor-pointer hover:opacity-70"
+              onClick={() => {
+                addNewTeamHandle();
+              }}
+            ></Add>
+          </span>
+          <span className="my-2 h-1 w-full bg-base-100" />
+          <h3 className="text-2xl">Personal</h3>
+        </div>
+        <div className="mb-8 flex h-min w-full flex-col">
+          <span className="flex flex-row items-center justify-between">
+            <h2 className="text-3xl">Categories</h2>
+            <Add
+              className="hover:cursor-pointer hover:opacity-70"
+              onClick={() => {
+                addNewCategoryHandle();
+              }}
+            ></Add>
+          </span>
+          <span className="my-2 h-1 w-full bg-base-100" />
+        </div>
       </nav>
       <div id="themechange" className="mt-auto">
         <span className="divider" />
@@ -24,7 +63,7 @@ const Sidebar: FC = () => {
               onClick={() => {
                 setTheme(theme);
               }}
-              className="btn-secondary btn-sm btn"
+              className="btn btn-secondary btn-sm"
             >
               {theme}
             </button>
