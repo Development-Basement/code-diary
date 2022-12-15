79f70d8d6d4abd36ce5ff4f45743420279d0d610
xhrnca00 <89995247+xhrnca00@users.noreply.github.com>
2022-12-15 23:29:15 +0100

    add redirects and loading states

--- pages/login.tsx
+++ pages/login.tsx
@@ -9,6 +9,7 @@ import { useRef, useState } from "react";
 
 import { FormSubmitHandler } from "@lib/types";
 
+import Spinner from "@components/spinner";
 import {
   useSendPasswordResetEmail,
   useSignInWithEmailAndPassword,
@@ -17,78 +18,28 @@ import {
 export default function Login() {
   const [resetModalOpen, setResetModalOpen] = useState(false);
 
-  const emailRef = useRef<HTMLInputElement>(null);
+  const resetEmailRef = useRef<HTMLInputElement>(null);
 
   const loginEmailRef = useRef<HTMLInputElement>(null);
   const loginPasswordRef = useRef<HTMLInputElement>(null);
 
-  const [sendPasswordResetEmail, loading, error] =
+  const [sendPasswordResetEmail, passwordLoading, passwordError] =
     useSendPasswordResetEmail(auth);
 
   const handlePasswordResetSubmit: FormSubmitHandler = (e) => {
     e.preventDefault();
-    sendPasswordResetEmail(emailRef.current!.value);
+    sendPasswordResetEmail(resetEmailRef.current!.value);
   };
 
-  const [login, loginLoading, loginError] = useSignInWithEmailAndPassword(auth);
+  const [login, , loginLoading, loginError] =
+    useSignInWithEmailAndPassword(auth);
 
   return (
     <>
-      <input
-        type="checkbox"
-        className="modal-toggle"
-        checked={resetModalOpen}
-        readOnly
-      />
-      <div className="modal">
-        <div className="modal-box relative flex flex-col">
-          <button
-            className="btn-sm btn-circle btn absolute right-2 top-2 p-1"
-            onClick={(e) => {
-              e.preventDefault();
-              setResetModalOpen(false);
-            }}
-          >
-            <CloseRoundedIcon className="h-full w-full" />
-          </button>
-          <h2 className="text-2xl font-bold">Reset password</h2>
-          <span className="divider my-1" />
-          <form onSubmit={handlePasswordResetSubmit}>
-            <input
-              type="email"
-              placeholder="account email"
-              className="input-bordered input-primary input w-full"
-              ref={emailRef}
-              required
-              autoFocus={resetModalOpen}
-            />
-            {error ? (
-              <div className="alert alert-error mt-3">
-                <p className="block overflow-hidden text-ellipsis whitespace-normal break-words">
-                  {error.message}
-                </p>
-              </div>
-            ) : (
-              <></>
-            )}
-            <span className="mt-2 flex w-full justify-end">
-              <button
-                className="btn-primary btn"
-                disabled={loading}
-                type="submit"
-              >
-                Send email
-              </button>
-            </span>
-          </form>
-        </div>
-      </div>
       <main className="flex h-screen flex-col place-items-center justify-center bg-base-100 pb-32 pt-16">
         <span className="flex flex-col place-items-center gap-1">
           <Logo rem={4} />
-          <desc className="mb-7 text-sm">
-            Keep track of your coding progress!
-          </desc>
+          <p className="mb-7 text-sm">Keep track of your coding progress!</p>
         </span>
         <div className="flex w-1/3 flex-col place-items-center gap-2">
           <form
@@ -124,9 +75,28 @@ export default function Login() {
             <button
               type="submit"
               className="btn-primary btn-md btn mt-3 w-full"
+              disabled={loginLoading}
             >
-              Login
+              {loginLoading ? (
+                <span className="flex place-items-center">
+                  <span className="mr-2 h-4 w-4">
+                    <Spinner />
+                  </span>
+                  Loading...
+                </span>
+              ) : (
+                "Login"
+              )}
             </button>
+            {loginError ? (
+              <div className="alert alert-error mt-3">
+                <p className="block overflow-hidden text-ellipsis whitespace-normal break-words">
+                  {loginError.message}
+                </p>
+              </div>
+            ) : (
+              <></>
+            )}
           </form>
           <button
             className="link-primary link mt-3 no-underline"
@@ -134,7 +104,7 @@ export default function Login() {
               e.preventDefault();
               setResetModalOpen(true);
               setTimeout(() => {
-                emailRef.current!.focus();
+                resetEmailRef.current!.focus();
               }, 50);
             }}
           >
@@ -146,6 +116,57 @@ export default function Login() {
           </Link>
         </div>
       </main>
+      <div id="password-modal">
+        <input
+          type="checkbox"
+          className="modal-toggle"
+          checked={resetModalOpen}
+          readOnly
+        />
+        <div className="modal">
+          <div className="modal-box relative flex flex-col">
+            <button
+              className="btn-sm btn-circle btn absolute right-2 top-2 p-1"
+              onClick={(e) => {
+                e.preventDefault();
+                setResetModalOpen(false);
+              }}
+            >
+              <CloseRoundedIcon className="h-full w-full" />
+            </button>
+            <h2 className="text-2xl font-bold">Reset password</h2>
+            <span className="divider my-1" />
+            <form onSubmit={handlePasswordResetSubmit}>
+              <input
+                type="email"
+                placeholder="account email"
+                className="input-bordered input-primary input w-full"
+                ref={resetEmailRef}
+                required
+                autoFocus={resetModalOpen}
+              />
+              {passwordError ? (
+                <div className="alert alert-error mt-3">
+                  <p className="block overflow-hidden text-ellipsis whitespace-normal break-words">
+                    {passwordError.message}
+                  </p>
+                </div>
+              ) : (
+                <></>
+              )}
+              <span className="mt-2 flex w-full justify-end">
+                <button
+                  className="btn-primary btn"
+                  disabled={passwordLoading}
+                  type="submit"
+                >
+                  Send email
+                </button>
+              </span>
+            </form>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
